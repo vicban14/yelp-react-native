b7b9c8e46964084d2ba4f51d74b8a725fc71bbc8
Víctor Bañuls <vicbansax@gmail.com>
2020-07-18 17:36:01 +0200

    Handle error

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ import { API_KEY, BASE_URL } from '@env'
 export default axios.create({
   baseURL: BASE_URL,
   headers: {
-    Authorization: `Bearer ${API_KEY}`,
+    Authorization: `Bearer${API_KEY}`,
   },
 })
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,16 +6,21 @@ import yelp from '../api/yelp'
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
   const [result, setResult] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
 
   async function searchApi() {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose',
-      },
-    })
-    console.log(response)
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'san jose',
+        },
+      })
+      setResult(response.data.businesses)
+    } catch (err) {
+      setErrorMessage('Something went wrong!')
+    }
   }
 
   return (
@@ -25,7 +30,7 @@ const SearchScreen = () => {
         onTermChange={(newTerm) => setTerm(newTerm)}
         onTermSubmitting={() => searchApi()}
       />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {result.length} businesses</Text>
     </View>
   )
